0ca2525e4880bad55f691b61e8bc896144da368c
V1def <vaniya8871@gmail.com>
2022-04-06 17:09:35 +0300

    Fix license bug

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 /*
  * Copyright © 2021-2022 V1def
 
- * This file is part of Durudex: you can redistribute it and/or modify
+ * This file is part of V1def: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
 
- * Durudex is distributed in the hope that it will be useful,
+ * V1def is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Durudex. If not, see <https://www.gnu.org/licenses/>.
+ * along with V1def. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package main
